cd6f91d63e59bd18bf763e26dc60932ddec103dc
Casey Bivens <caseyoksana@gmail.com>
2020-07-20 20:53:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  living_rm.push("Records")
+  living_rm.push ("Records")
 end
 
 def add_element_to_start_of_array(array, element)
